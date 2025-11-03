f370081a6ac831981c7e9cd070042d50f0e91548
lucco17 <lucbaz17@gmail.com>
2025-11-02 20:09:34 -0500

    Recommitting everything so it ignores txt
    I removed all the cached files, and now recommitting everything without the txt files as they include the inputs

--- 2021/19/code.py
+++ 2021/19/code.py
@@ -1,3 +1,14 @@
-with open('input.txt') as f:
+import numpy as np
+
+scanners = []
+with open('input_small.txt') as f:
 	for li in f.readlines():
 		li = li.removesuffix('\n')
+		if li == '--- scanner 0 ---':
+			curr_scanner = []
+		elif li.startswith('--- scanner'):
+			scanners.append(np.array(curr_scanner))
+			curr_scanner = []
+		elif li != '':
+			curr_scanner.append([int(num) for num in li.split(',')])
+scanners.append(np.array(curr_scanner))
